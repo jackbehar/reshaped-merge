435664625908c420267d3390e9d5374441b1fdb2
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-05-31 17:00:41 -0600

    cleanup

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,18 @@
-import React, { FC, } from 'react';
+import React, { ComponentType, FC, ReactNode} from 'react';
 import { Button as ButtonM, ButtonProps } from 'reshaped';
-
-export interface ButtonPropsMerge
-  extends ButtonProps {
-  tailwindClasses?: string
+/**
+ * @uxpindocurl https://reshaped.so/docs/components/button
+ * @uxpindescription Interactive element used for single-step actions.
+ */
+export interface ButtonPropsMerge extends ButtonProps {
+  children?: ReactNode,
+  icon?: ComponentType,
+  endIcon?: ComponentType,
+  
+  /**
+  * Example of a description and additional prop.
+  */
+  extendedPropExample?: string
 }
 
 const Button: FC = (props: ButtonPropsMerge) => {
