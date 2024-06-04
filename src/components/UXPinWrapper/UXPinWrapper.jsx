3a3d70147f10ab725565f84b3c049849649b2201
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2024-06-04 13:31:57 -0600

    cleanup

--- src/components/UXPinWrapper/UXPinWrapper.jsx
+++ src/components/UXPinWrapper/UXPinWrapper.jsx
@@ -1,9 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { Reshaped } from "reshaped";
-import "./customTheme.css"
-
+import "./customTheme.css";
 
 export default function UXPinWrapper({ children }) {
-  return  <div style={{ display: "grid" }}><Reshaped theme="customTheme" defaultColorMode="light">{children}</Reshaped></div>;
+  return (
+    <div style={{ display: "grid" }}>
+      <Reshaped theme="customTheme" defaultColorMode="light">
+        {children}
+      </Reshaped>
+    </div>
+  );
 }
